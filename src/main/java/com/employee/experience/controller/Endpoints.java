346ca9e6f7fafa9ec24159abc7b3771d1ff548c0
BalazsParducz <parducz.balazs@gmail.com>
2019-03-12 12:59:25 +0100

    removed controller for saving experience

--- src/main/java/com/employee/experience/controller/Endpoints.java
+++ src/main/java/com/employee/experience/controller/Endpoints.java
@@ -2,8 +2,6 @@ package com.employee.experience.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Endpoints {
@@ -14,8 +12,4 @@ public class Endpoints {
         return "index";
     }
 
-    @PostMapping("/saveExperience/{userId}/{mood}")
-    public String saveExperience(@RequestParam String userId, @RequestParam String mood) {
-        return userId + " " + mood;
-    }
 }
