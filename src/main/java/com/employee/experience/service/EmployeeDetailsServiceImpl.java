ea42ce4c114c52b02e51042d5c850847bc0412a3
BalazsParducz <parducz.balazs@gmail.com>
2019-03-13 13:20:55 +0100

    implemented UserDetailsService interface's method in concrete class

---
+++ src/main/java/com/employee/experience/service/EmployeeDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.employee.experience.service;
+
+import com.employee.experience.model.Employee;
+import com.employee.experience.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static java.util.Collections.emptyList;
+
+@Service
+public class EmployeeDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+    public EmployeeDetailsServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Employee employee = employeeRepository.findByEmail(email);
+        if (employee == null) {
+            throw new UsernameNotFoundException(email);
+        }
+        return new User(employee.getEmail(), employee.getPassword(), emptyList());
+    }
+}
