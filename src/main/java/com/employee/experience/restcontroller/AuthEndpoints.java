1d5d6d0f7dde674cbb33a81e43ae241903072566
BalazsParducz <parducz.balazs@gmail.com>
2019-03-13 13:13:39 +0100

    added registration related controller

---
+++ src/main/java/com/employee/experience/restcontroller/AuthEndpoints.java
@@ -0,0 +1,30 @@
+package com.employee.experience.restcontroller;
+
+
+import com.employee.experience.model.Employee;
+import com.employee.experience.repository.EmployeeRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+public class AuthEndpoints {
+
+    private EmployeeRepository employeeRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public AuthEndpoints(EmployeeRepository employeeRepository,
+                          BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.employeeRepository = employeeRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @PostMapping("/register")
+    public void registerNewEmployee(@RequestBody Employee newEmployee) {
+        newEmployee.setPassword(bCryptPasswordEncoder.encode(newEmployee.getPassword()));
+        employeeRepository.save(newEmployee);
+    }
+}
